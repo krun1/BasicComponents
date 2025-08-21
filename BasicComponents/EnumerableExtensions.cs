3cdd3414b0dc7819f8c7429fd752c69a2d0f3929
Adrien <adrien.emery.19@gmail.com>
2025-08-21 10:25:18 +0200

    Add extensions

---
+++ BasicComponents/EnumerableExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace BasicComponents;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> source) => source.Where(x => x != null)!;
+    
+    public static void SmartForeach<T>(this IEnumerable<T> source,
+        Action<T>? first = null,
+        Action<T>? each = null,
+        Action<T>? last = null)
+    {
+        ArgumentNullException.ThrowIfNull(each);
+        using var e = source.GetEnumerator();
+
+        if (!e.MoveNext() && (first != null || last != null))
+            throw new InvalidOperationException("Sequence contains no elements");
+        if (first != null)
+        {
+            first(e.Current);
+            if (!e.MoveNext() && last != null)
+                throw new InvalidOperationException("Sequence do not contain enough elements to call first and last");
+        }
+        var c = e.Current;
+
+        while (e.MoveNext())
+        {
+            each.Invoke(c);
+            c = e.Current;
+        }
+
+        if (last != null)
+            last(c);
+        else
+            each.Invoke(c);
+    }
+}
---
+++ BasicComponents/TypeExtensions.cs
@@ -0,0 +1,42 @@
+﻿namespace BasicComponents;
+
+public static class TypeExtensions
+{
+    public static bool IsSubclassOfGenericInterface(this Type type, Type genericDefinition)
+    {
+        if (!genericDefinition.IsGenericTypeDefinition)
+            throw new ArgumentException($"Type {genericDefinition} is not a generic type definition");
+        if (type.IsGenericType && type.GetGenericTypeDefinition() == genericDefinition)
+            return true;
+        return type.GetInterfaces()
+            .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == genericDefinition);
+    }
+
+    public static Type[] GetGenericParametersOfGenericInterface(this Type type, Type genericDefinition)
+    {
+        if (!genericDefinition.IsGenericTypeDefinition)
+            throw new ArgumentException($"Type {genericDefinition} is not a generic type definition");
+        var i = type.GetInterfaces()
+            .Single(i => i.IsGenericType && i.GetGenericTypeDefinition() == genericDefinition);
+
+        return i.GetGenericArguments();
+    }
+    
+    public static bool IsSubclassOfGenericClass(this Type type, Type genericDefinition)
+    {
+        if (!genericDefinition.IsGenericTypeDefinition)
+            throw new ArgumentException($"Type {genericDefinition} is not a generic type definition");
+        if (type.GetGenericTypeDefinition() == genericDefinition)
+            return true;
+        if (type == typeof(object) || type.BaseType == null)
+            return false;
+        return type.BaseType.IsSubclassOfGenericClass(genericDefinition);
+    }
+
+    public static bool ImplementInterface(this Type type, Type @interface)
+    {
+        if (!@interface.IsInterface)
+            throw new ArgumentException($"Type {@interface} is not an interface");
+        return type.GetInterfaces().Contains(@interface);
+    }
+}
