a8b0b1ccfe09848c6de54411eda0bf2c95c0b1cd
Adrien <adrien.emery.19@gmail.com>
2026-07-27 18:01:31 +0900

    [BasicComponents] Update DataOrError

--- BasicComponents/Monad/DataOrError.cs
+++ BasicComponents/Monad/DataOrError.cs
@@ -1,17 +1,29 @@
 ﻿namespace BasicComponents.Monad;
 
-public class DataOrError<T>(T value, string error, bool isValid)
+public class DataOrError<T>(T value, Exception error, bool isValid)
 {
     public bool IsValid { get; } = isValid;
-    public T Value => IsValid ? value : throw new InvalidOperationException($"Value is not valid. Error : {error}");
-    public string Error => IsValid ? throw new InvalidOperationException($"No Error available") : error;
+    public T Value => IsValid ? value : throw new InvalidOperationException($"Value is not valid.", error);
+    public Exception Error => IsValid ? throw new InvalidOperationException($"No Error available") : error;
     
-    public static implicit operator DataOrError<T>(T value) => new(value, "", true);
+    public static implicit operator DataOrError<T>(T value) => new(value, null!, true);
 }
 
 public static class DataOrError
 {
-    public static DataOrError<T> Create<T>(T value) => new(value, "", true);
-    
-    public static DataOrError<T> Error<T>(string error) => new(default, error, false);
+    public static DataOrError<T> Create<T>(T value) => new(value, null!, true);
+    public static DataOrError<T> Error<T>(Exception error) => new(default!, error, false);
+    public static DataOrError<T> Error<T>(string error) => new(default!, new Exception(error), false);
+
+    public static DataOrError<T> Try<T>(Func<T> func)
+    {
+        try
+        {
+            return func();
+        }
+        catch (Exception e)
+        {
+            return Error<T>(e);
+        }
+    }
 }
---
+++ BasicComponents/Monad/DataOrErrorExtension.cs
@@ -0,0 +1,7 @@
+﻿namespace BasicComponents.Monad;
+
+public static class DataOrErrorExtension
+{
+    public static DataOrError<TResult> Then<T, TResult>(this DataOrError<T> value, Func<T, TResult> func)
+        => value.IsValid ? DataOrError.Try(() => func(value.Value)) : DataOrError.Error<TResult>(value.Error);
+}
