151e7dec059d9b7efc12550a5543fb1a7ebc6caa
Adrien <adrien.emery.19@gmail.com>
2026-08-14 20:50:56 +0900

    [Monad] Check add async

--- BasicComponents/Monad/CheckExtension.cs
+++ BasicComponents/Monad/CheckExtension.cs
@@ -2,9 +2,20 @@ namespace BasicComponents.Monad;
 
 public static class CheckExtension
 {
-    public static DataOrError<T> Then<T>(this Check check, Func<T> value)
+    public static DataOrError<T> Select<T>(this Check check, Func<T> value)
         => check.IsValid ? DataOrError.Create(value()) : DataOrError.Error<T>(check.Error);
 
     public static DataOrError<T> Then<T>(this Check check, Func<DataOrError<T>> value)
         => check.IsValid ? value() : DataOrError.Error<T>(check.Error);
+}
+
+public static class CheckAsyncExtension
+{
+    public static async Task<DataOrError<T>> SelectAsync<T>(this Check check, Func<Task<T>> value)
+        => check.IsValid ? DataOrError.Create(await value()) : DataOrError.Error<T>(check.Error);
+
+    public static async Task<DataOrError<T>> ThenAsync<T>(this Check check, Func<Task<DataOrError<T>>> value)
+    {
+        return check.IsValid ? await value() : DataOrError.Error<T>(check.Error);
+    }
 }
